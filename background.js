23d260d8a0356dfb49ae1a862b91d0ff8d48f29b
sksareen <sksareen1@outlook.com>
2024-08-01 17:05:47 -0400

    improved the function, colors, look and feel. ready to publish v1

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, {action: "toggleBreather"});
-  console.log("background loaded");
+  if (!tab || !tab.url || tab.url.startsWith('chrome://')) {
+    chrome.tabs.create({ url: 'https://www.google.com' }, (newTab) => {
+      setTimeout(() => {
+        chrome.tabs.sendMessage(newTab.id, { action: "showOverlay" });
+      }, 1000); // Wait for the page to load
+    });
+  } else {
+    chrome.tabs.sendMessage(tab.id, { action: "showOverlay" });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "updateState") {
+    chrome.storage.local.set(request.state);
+  }
 });
